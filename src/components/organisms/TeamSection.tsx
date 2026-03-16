74f6b30b34930b8dabcbb1ad8e0bde2ca6b480bf
Rinesa Smajli <rinesasmajli11@gmail.com>
2026-03-16 21:07:06 +0100

    add

--- src/components/organisms/TeamSection.tsx
+++ src/components/organisms/TeamSection.tsx
@@ -20,7 +20,7 @@ const TeamSection: React.FC = () => {
         },
         {
             name: "Rina Hoti",
-            role: "Graphic Designer",
+            role: "Software Engineer",
             image: "/rina.jpeg",
             description: "Specialist in visual communication, branding, and creative design for digital and print media."
         },
